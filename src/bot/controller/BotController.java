5478dcb6ab677e9e7e599863bc814b06f4013077
iceflower99 <mhal4767@740s-pl-120.local>
2015-12-16 09:03:15 -0700

    added new stuff

---
+++ src/bot/controller/BotController.java
@@ -0,0 +1,18 @@
+package bot.controller;
+
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+import bot.model.EV3Bot;
+
+public class BotController
+{
+   
+	private String message;
+	private int xPositon,yPosition;
+	private long waitTime;
+	public void start()
+    {
+    	
+    }
+}
---
+++ src/bot/controller/MaddyRunner.java
@@ -0,0 +1,10 @@
+package bot.controller;
+
+public class MaddyRunner
+{
+    public static void main(String[]args)
+    {
+    	    BotController myBotController =new BotController();
+    	 myBotController.start();		
+    }
+}
